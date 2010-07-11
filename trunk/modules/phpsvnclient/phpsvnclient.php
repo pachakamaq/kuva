2ebe96ceb0d8836e0b969733adb85935835459ad
jesdisciple <jesdisciple@3d78a3a7-9da0-e71b-15eb-3d56f06a2f09>
2010-07-11 16:45:58 +0000

    Fixed the phpsvnclient module but not having it overwrite Kuva just yet.

--- trunk/modules/phpsvnclient/phpsvnclient.php
+++ trunk/modules/phpsvnclient/phpsvnclient.php
@@ -1,12 +1,17 @@
 <?php
     $dir = dirname(__FILE__);
-    require $dir . '/phpsvnclient_patch.php';
+    require $_SERVER['DOCUMENT_ROOT'] . '/phpsvnclient/phpsvnclient.php';
     $sources = array();
     $dom = extractElements($dir . '/config.xml', $sources);
     foreach($sources['SOURCE'] as $source){
         $user = $source->hasAttribute('user') ? $source->getAttribute('user') : NULL;
         $pass = $source->hasAttribute('pass') ? $source->getAttribute('pass') : NULL;
         $phpsvnclient = new phpsvnclient($source->getAttribute('url'), $user, $password);
-        $phpsvnclient->checkOut('trunk', $source->getAttribute('local'));
+        for($i = 0; $i < $source->childNodes->length; ++$i){
+            if($source->childNodes->item($i)->nodeType == 1){
+                $dir = $source->childNodes->item($i);
+                $phpsvnclient->checkOut($dir->getAttribute('remote'), $dir->getAttribute('local'));
+            }
+        }
     }
 ?>
